47c97ed0b64b5d9142ef24627971e984708d9593
DavSCo <david.cohen@ynov.com>
2018-04-18 16:46:02 +0200

    scss

--- digit-webpack/src/components/Card.vue
+++ digit-webpack/src/components/Card.vue
@@ -26,52 +26,6 @@ export default {
 }
 </script>
 
-
-
-<style  scoped>
-.article {
-  border-radius: 0 !important;
-}
-
-.image-article {
-  border-top-left-radius: 0 !important;
-  border-top-right-radius: 0 !important;
-}
-.body-article {
-  background-color: #2e3443;
-}
-.grille-text {
-  font-family: "Roboto", sans-serif;
-  color: #d4eee2;
-  text-align: end;
-}
-.grille-title {
-  font-family: "Roboto", sans-serif;
-  color: #d4eee2;
-}
-gallery {
-  -webkit-column-count: 4;
-  -moz-column-count: 4;
-  column-count: 4;
-  -webkit-column-gap: 10px;
-  -moz-column-gap: 10px;
-  column-gap: 10px;
-  margin-top: 10px;
-  overflow: hidden;
-}
-
-.gallery .gallery-img-container {
-  width: 100%;
-  height: auto;
-  transition: 500ms;
-  margin-bottom: 10px;
-  page-break-inside: avoid; /* For Firefox. */
-  -webkit-column-break-inside: avoid; /* For Chrome & friends. */
-  break-inside: avoid; /* For standard browsers like IE. :-) */
-}
-
-.gallery .gallery-img-container:hover {
-  opacity: 0.80;
-}
+<style lang="scss">
+  @import '../assets/card.scss';
 </style>
-
